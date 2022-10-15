75e94bf40fa0efdb924bf619684bdf8b0027f3df
GloriaMwakai <glowanjala@gmail.com>
2022-10-15 00:52:35 -0700

    use of function puts to print text

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 
+/**
+ * Main - prints the phrase "Programming is like building a multilingual puzzle
+ * Return: 0 if exited properly, non-zero otherwise
+ */
+
 int main(void)
-
 {
-
-	puts("Programming is like building a multilingual puzzle\n");
-
+	puts("\"Programming is like a multilingual puzzle");
 	return (0);
 
 }
